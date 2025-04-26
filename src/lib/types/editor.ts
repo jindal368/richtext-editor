7b2dab22022fd45f5b257de94a3a30482931a8f3
jindal368 <visheshjindal368@gmail.com>
2025-04-26 18:59:31 +0530

    Initial commit from Create Next App

---
+++ src/lib/types/editor.ts
@@ -0,0 +1,60 @@
+export type TextFormatting = {
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  heading?: 1 | 2 | 3 | 4 | 5 | 6;
+};
+
+export type BlockType =
+  | "paragraph"
+  | "quote"
+  | "code"
+  | "callout"
+  | "list-item";
+
+export interface TextNode {
+  type: "text";
+  text: string;
+  formatting: TextFormatting;
+}
+
+export interface InteractiveNode {
+  type: "interactive";
+  componentType: string;
+  props: Record<string, any>;
+}
+
+export interface BlockNode {
+  type: BlockType;
+  children: (TextNode | InteractiveNode)[];
+  indent?: number;
+}
+
+export interface EditorState {
+  blocks: BlockNode[];
+  selection: {
+    start: { block: number; offset: number };
+    end: { block: number; offset: number };
+    isCollapsed: boolean;
+  };
+  undoStack: EditorState[];
+  redoStack: EditorState[];
+}
+
+export interface EditorCommand {
+  id: string;
+  label: string;
+  shortcut?: string;
+  execute: (state: EditorState) => EditorState;
+}
+
+export interface MentionData {
+  id: string;
+  type: string;
+  label: string;
+  preview?: {
+    title: string;
+    description?: string;
+    imageUrl?: string;
+  };
+}
